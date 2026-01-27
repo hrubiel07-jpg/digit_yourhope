957e595d847da8786601990cf2db613ea7e41a97
hrubiel07-jpg <hrubiel07@gmail.com>
2026-01-27 17:01:19 +0100

    Add files via upload

--- configurations.php
+++ configurations.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../includes/config.php';
-require_once '../../../includes/school_config.php';
+require_once __DIR__ . '/../../includes/school_config.php';
 requireLogin();
 
 if ($_SESSION['user_type'] !== 'school') {
